4aa2d79740ec4844881ea0d9e34ff89e2d6e85e9
Joakim Kainulainen <joakimkainulainen@MBP1123.lan>
2024-10-06 14:33:28 +0300

    first commit

---
+++ src/Tabs.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Box, Tabs, Tab } from '@mui/material';
+import TodoList from './TodoList';
+
+
+function MyTabs() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} >
+          <Tab label="Welcome"  />
+          <Tab label="TodoList"  />
+        </Tabs>
+      </Box>
+
+      {value == 0 &&
+        <h1>Welcome to the Todo List App!</h1>}
+
+      {value == 1 &&
+        <TodoList />}
+    </>
+  );
+}
+
+export default MyTabs;
